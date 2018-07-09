66122cbe0dabcf8ed24ef08a1d8ab05aeb2ca0c3
IvanSeguljev <ivan26516@its.edu.rs>
2018-07-09 20:43:30 +0200

    Lista sad nema hardcode

--- resources/views/Administracija/Lista.blade.php
+++ resources/views/Administracija/Lista.blade.php
@@ -24,20 +24,7 @@
             <td>Row 1 Data 1</td>
         </tr>
         @endforeach
-        <tr>
-            <td>Row 1 Data 1</td>
-            <td>Row 1 Data 2</td>
-            <td>Row 1 Data 1</td>
-            <td>Row 1 Data 1</td>
-            <td>Row 1 Data 1</td>
-        </tr>
-        <tr>
-            <td>Row 2 Data 1</td>
-            <td>Row 2 Data 2</td>
-            <td>Row 1 Data 1</td>
-            <td>Row 1 Data 1</td>
-            <td>Row 1 Data 1</td>
-        </tr>
+        
     </tbody>
 </table>
         </div>
