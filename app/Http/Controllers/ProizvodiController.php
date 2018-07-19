4bd1c7a216e7c559e4bf93c2b8b342f81579defc
IvanSeguljev <ivan26516@its.edu.rs>
2018-07-19 20:22:23 +0200

    Uradjeno pretrazivanje :D

--- app/Http/Controllers/ProizvodiController.php
+++ app/Http/Controllers/ProizvodiController.php
@@ -4,11 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Proizvod;
+use Illuminate\Support\Facades\Input;
+use App\Kategorija;
 class ProizvodiController extends Controller
 {
-public function Prikazi()
+public function Prikazi(Request $req)
 {
-    
-    return view('Proizvod.Prikazi')->with('proizvodi', Proizvod::paginate(9));
+    $proizvodi = Proizvod::where('id','>',0);
+    if($req->minimalnaCena!=null)
+    {
+    $proizvodi= $proizvodi->where('cenaPoKomadu','>=',$req->minimalnaCena);
+    }
+    if($req->maksimalnaCena!=null)
+    {
+    $proizvodi= $proizvodi->where('cenaPoKomadu','<=',$req->maksimalnaCena);
+    }
+    if($req->kategorija != 0)
+    {
+        $proizvodi=$proizvodi->where('kategorija_id','=',$req->kategorija);
+    }
+    if($req->samoNaStanju != null)
+    {
+        $proizvodi=$proizvodi->where('kolicina','>',0);
+    }
+    $proizvodi=$proizvodi->paginate(9);
+    return view('Proizvod.Prikazi')->with('proizvodi', $proizvodi)->with('kategorije', Kategorija::all());
 }
 }
--- resources/views/Proizvod/Prikazi.blade.php
+++ resources/views/Proizvod/Prikazi.blade.php
@@ -1,7 +1,56 @@
 <!doctype html>
 @extends('layouts.app')
+
 @section('content')
 <body>
+   
+        
+    <nav class="navbar navbar-expand-lg navbar-light bg-light" style="width:100%;     margin-left: 20px;">
+  <b class="navbar-brand">Pretraga</b>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  
+  <div class="collapse navbar-collapse" id="navbarColor03">
+      <form method="get" action="/Proizvodi/prikazi">
+    <ul class="navbar-nav mr-auto">
+     
+        <li class="nav-item dropdown active">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Pretraga po ceni
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <label>Minimalna cena:</label><input value="{{old('minimalnaCena')}}" type="number" name="minimalnaCena"/>
+         
+          <div class="dropdown-divider"></div>
+          <label>Maksimalna cena:</label><input type="number" name="maksimalnaCena"/>
+        </div>
+      </li>
+      <div style="width:10px;"></div>
+      <li class="nav-item">
+          <select class="form-control" name="kategorija">
+              <option selected="selected" value="0">Pretraga po kategotiji</option>
+               @foreach($kategorije as $kategorija)
+               <option value="{{$kategorija->id}}">{{$kategorija->naziv}}</option>
+               @endforeach
+      </select>
+      </li>
+      <div style="width:10px;"></div>
+      <li class="nav-item">
+          <div >
+          <input  type="checkbox" name="samoNaStanju"/>Prikazi samo proizvode na stanju
+          </div>
+      </li>
+      <div style="width:10px;"></div>
+      <li class="nav-item">
+      <button  class="btn btn-secondary navbar-btn" type="submit">Pretrazi</button>
+      </li>
+    </ul>
+    </form>
+  </div>
+    
+</nav>
+    
     <div class="row">
     @foreach($proizvodi as $proizvod)
     <div class="col-md-4">
