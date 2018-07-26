66331f2691e34d1fa8ee1ebe9a0deef5b6615baa
IvanSeguljev <ivan26516@its.edu.rs>
2018-07-26 23:22:08 +0200

    zadnji upload

--- resources/views/Administracija/PrikaziProizvod.blade.php
+++ resources/views/Administracija/PrikaziProizvod.blade.php
@@ -1,9 +1,9 @@
 <!doctype html>
 @extends('layouts.app')
 @section('content')
-<div class="text-center">
-    <div class="card col-md-4" style=" width: 18rem; display: inline-block; margin: 20px;">
-  <img class="card-img-top" src="{{asset("SlikeProizvoda/".$proizvod->slika)}}" alt="Card image cap">
+<div class="text-center col-md-4 offset-4">
+    <div class="card" style=" width: 18rem; display: inline-block;">
+        <img class="card-img-top" src="{{asset("SlikeProizvoda/".$proizvod->slika)}}"alt="Card image cap">
   <div class="card-body">
     <h5 class="card-title">{{$proizvod->naziv}}</h5>
     
@@ -18,6 +18,7 @@
 </div>
  
     <br>
-    <a href="/Administracija/Roba" ><button type="button" class="btn btn-danger form-control col-md-4">Povratak na pregled proizvoda</button></a>
+    
 </div>
+<a href="/Administracija/Roba" ><button type="button" class="btn btn-danger form-control col-md-4 offset-4">Povratak na pregled proizvoda</button></a>
 @endsection
